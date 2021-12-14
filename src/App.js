a7cc0697c7646c21a7e376c67ef1babd1084ba0f
Pedro Perondini <pedroperondini@gmail.com>
2021-12-13 23:14:35 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import Box from './components/box'
-import Axios from 'axios';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  let myHeaders = new Headers();
-  myHeaders.append("requestToken", "bcd40b09-6328-4335-9de9-fd91cbcf46e5");
 
-  let id = 1;
-  let url = "https://api-carros.herokuapp.com/api/Carro/GetId/" + id;
-  
-  let requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-  
-  fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  const [data, setData] = useState();
+
+   React.useEffect (function() {
+    axios.get("https://api-carros.herokuapp.com/api/Carro/GetId/1", {headers: {
+      "requestToken" : "bcd40b09-6328-4335-9de9-fd91cbcf46e5"
+    }})
+    .then(response => setData(response.data), console.log(data))
+    .catch(error => console.log(error))
+   })
+
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App"></div>
   );
 }
 
