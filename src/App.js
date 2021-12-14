7bead04a4a97ed28c00e10c1977688e026d040ee
Pedro Perondini <pedroperondini@gmail.com>
2021-12-13 22:26:30 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Box from './components/box'
+import Axios from 'axios';
+import axios from 'axios';
 
 function App() {
+  let myHeaders = new Headers();
+  myHeaders.append("requestToken", "bcd40b09-6328-4335-9de9-fd91cbcf46e5");
+
+  let id = 1;
+  let url = "https://api-carros.herokuapp.com/api/Carro/GetId/" + id;
+  
+  let requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch(url, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
