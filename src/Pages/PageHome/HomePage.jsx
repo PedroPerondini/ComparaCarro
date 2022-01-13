47aa7d5face5b3c824e4300715779e55c7276d87
Matheus Lima <matheus.la@hotmail.com>
2022-01-13 19:17:02 -0300

    Funfou

--- src/Pages/PageHome/HomePage.jsx
+++ src/Pages/PageHome/HomePage.jsx
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { Component } from "react";
 import './HomePage.css';
 import Header from '../components/header/header';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function HomePage () {
-
-
-    return (
-        <>
-            <Header/>
-            <div className='containerImage'>
-                <h1> IMAGEM DO CARRO </h1>
-            </div>
-            <div className='titulo'>
-                <h1> TITULO </h1>
-            </div>
-            <div className='footer'>
-                <div className='container'>
-                    <div className='audi-logo'>
-                        <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="audi-logo" srcset="" />
-                    </div>
-                    <div className='bugatti-logo'>
-                        <img src="https://www.carlogos.org/logo/Bugatti-logo-1024x768.png" alt="bugatti-logo" srcset="" />
-                    </div>
-                    <div className='chevy-logo'>
-                        <img src="https://www.carlogos.org/logo/Chevrolet-logo-2013-2560x1440.png" alt="chevy-logo" srcset="" />
-                    </div>
+export default class HomePage extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <>
+                <Header/>
+                <div className='containerImage'>
+                    <h1> IMAGEM DO CARRO </h1>
                 </div>
-            </div>
-        </>
-    );
+                <div className='titulo'>
+                    <h1> TITULO </h1>
+                </div>
+                <div className='footer'>
+                    <div className="container"> 
+                        <Slider {...settings}>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div>
+                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                            </div>
+                        </Slider>
+                    </div>
+                </div> 
+            </>
+        );
+    }
 }
+
+
