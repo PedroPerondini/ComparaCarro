6b2dc14a7b99475f0e1212393906b739bd1af116
Matheus Lima <matheus.la@hotmail.com>
2022-01-13 18:50:15 -0300

    Testaesssee au zpe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import RouteComponent from './route';
 
 
+
+
 export default function App() {
   return (
     <div className='App'>
--- src/Pages/PageComparaCarro/ComparaCarro.jsx
+++ src/Pages/PageComparaCarro/ComparaCarro.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
 import './ComparaCarro.css';
 import Header from '../components/header/header';
+import Slider from "react-slick";
 
 
-export default function ComparaCarro () {
-
-    return (
-        <>
-            <Header/>
-            <h1> Comparra Carro Page </h1>
-            
-        </>
-    );
-
-}
+export default class ComparaCarro extends Component {
+    render() {
+      const settings = {
+        dots: true,
+        fade: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      return (
+        <div>
+          <h2>Fade</h2>
+          <Slider {...settings}>
+            <div>
+              <h1> PEDRO </h1>
+            </div>
+            <div>
+              <h1> JOSE </h1>
+            </div>
+            <div>
+              <h1> AUGUSTU </h1>
+            </div>
+            <div>
+              <h1> ZÉ </h1>
+            </div>
+          </Slider>
+        </div>
+      );
+    }
+  }
