8021051a7bf468450663f48866202e0aeb370931
Pedro Perondini <pedroperondini@gmail.com>
2022-01-13 20:38:26 -0300

    Remove local images + some tests

--- src/Pages/PageHome/HomePage.jsx
+++ src/Pages/PageHome/HomePage.jsx
@@ -4,6 +4,7 @@ import Header from '../components/header/header';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ImageGradient from "material-ui/svg-icons/image/gradient";
 
 export default class HomePage extends Component {
     render() {
@@ -11,9 +12,18 @@ export default class HomePage extends Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            className: "center"
         };
+
+        function imageResize ()
+        {
+            let images = document.getElementsByClassName('container');
+            images.style.width = '1920px';
+            images.stye.height = '1080px';
+        }
+
         return (
             <>
                 <Header/>
@@ -24,25 +34,25 @@ export default class HomePage extends Component {
                     <h1> TITULO </h1>
                 </div>
                 <div className='footer'>
-                    <div className="container"> 
+                    <div className="container" onLoad={imageResize()}> 
                         <Slider {...settings}>
                             <div>
                                 <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
                             </div>
                             <div>
-                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                                <img src="https://www.carlogos.org/logo/Bugatti-logo-1024x768.png" alt="" srcset="" />
                             </div>
                             <div>
-                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                                <img src="https://www.carlogos.org/logo/Chevrolet-logo-2004-1920x1080.jpg" alt="" srcset="" />
                             </div>
                             <div>
-                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                                <img src="https://www.carlogos.org/logo/Dodge-logo-1990-2100x2100.png" alt="" srcset="" />
                             </div>
                             <div>
-                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                                <img src="https://www.carlogos.org/logo/Eagle-automobile-logo-1920x1080.png" alt="" srcset="" />
                             </div>
                             <div>
-                                <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
+                                <img src="https://www.carlogos.org/car-logos/scuderia-ferrari-logo-800x1050.png" alt="" srcset="" />
                             </div>
                         </Slider>
                     </div>
