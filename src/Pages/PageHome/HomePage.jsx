4eba378c7b924ac59d38f6622dda2a71e628b4c0
Pedro Perondini <pedroperondini@gmail.com>
2022-01-14 00:03:36 -0300

    Title adjustment

--- src/Pages/PageHome/HomePage.jsx
+++ src/Pages/PageHome/HomePage.jsx
@@ -12,7 +12,7 @@ export default class HomePage extends Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 4,
+            slidesToShow: 5,
             slidesToScroll: 1,
             className: "center"
         };
@@ -24,28 +24,34 @@ export default class HomePage extends Component {
                     <h1> IMAGEM DO CARRO </h1>
                 </div>
                 <div className='titulo'>
-                    <h1> TITULO </h1>
+                    <h1>Compara Carros</h1>
                 </div>
                 <div className='footer'>
                     <div className="container"> 
                         <Slider {...settings}>
-                            <div>
+                            <div className="audi">
                                 <img src="https://www.carlogos.org/logo/Audi-logo-2009-1920x1080.png" alt="" srcset="" />
                             </div>
-                            <div>
+                            <div className="bugatti">
                                 <img src="https://www.carlogos.org/logo/Bugatti-logo-1024x768.png" alt="" srcset="" />
                             </div>
-                            <div>
+                            <div className="chevrolet">
                                 <img src="https://www.carlogos.org/logo/Chevrolet-logo-2004-1920x1080.jpg" alt="" srcset="" />
                             </div>
-                            <div>
-                                <img src="https://www.carlogos.org/logo/Dodge-logo-1990-2100x2100.png" alt="" srcset="" />
+                            <div className="dodge">
+                                <img src="https://bn1305files.storage.live.com/y4madApCk5R3_Wgiqw34BwEkaRjVEqL1xFG43qaeRHzx-QtQkW6UW7GbVE7rGIZy7Q9CLoR8WLZ24U7viDXMxXz1iPN-50Ew--dd7P-HrTrDXJEaBoGaHcprmJ2tDf4fn1pa_hNinGod1EusCzLqXaa4mO4MK0aMfWow-BDK2SZBOPbDoZi4DHPu1j-hYlLfPik?width=1920&height=1080&cropmode=none" alt="" srcset="" />
                             </div>
-                            <div>
+                            <div className="eagle">
                                 <img src="https://www.carlogos.org/logo/Eagle-automobile-logo-1920x1080.png" alt="" srcset="" />
                             </div>
-                            <div>
-                                <img src="https://www.carlogos.org/car-logos/scuderia-ferrari-logo-800x1050.png" alt="" srcset="" />
+                            <div className="ferrari">
+                                <img src="https://www.carlogos.org/car-logos/ferrari-text-logo-1600x400.png" alt="" srcset="" />
+                            </div>
+                            <div className="gumpert">
+                                <img src="https://www.carlogos.org/logo/Gumpert-logo-1920x1080.png" alt="" srcset="" />
+                            </div>
+                            <div className="honda">
+                                <img src="https://bn1305files.storage.live.com/y4mis1dmIZeYBiJByBNJQQ4POKpYkK8pqDQ9hR6fRxT51I9zTDGcoqCjAH2njujIT-dgHzNV81iiCVknLnlOIMBLO5iXP2lSkNiByqp5O--ZtUWQi-k3T6t1NylZd3HJuF5FL6aLa-HSLZoy0ibNlMDzGSZ-0lQH2zzSEqI3PCoy_5WQwqb-LiLiJQXyWBwHhgf?width=1920&height=1080&cropmode=none" alt="" srcset="" />
                             </div>
                         </Slider>
                     </div>
@@ -54,5 +60,3 @@ export default class HomePage extends Component {
         );
     }
 }
-
-
